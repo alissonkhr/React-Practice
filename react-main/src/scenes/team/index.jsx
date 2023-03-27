9b00a7a710c3dd895e3fbd7ce2fe81e114c18386
alissonkhr <alissonricardo68@gmail.com>
2023-03-27 00:15:48 -0700

    imports added for team component

---
+++ react-main/src/scenes/team/index.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlined from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from "../../components/Header";
+
+const Team = () => {
+  return <div>Team</div>;
+};
+
+export default Team;
