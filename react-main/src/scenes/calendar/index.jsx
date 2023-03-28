cdc35e524f6182e70bedb6df3dc1bfef1f88a1a3
alissonkhr <alissonricardo68@gmail.com>
2023-03-28 13:26:05 -0700

    events sidebar for calendar basic set up populates

--- react-main/src/scenes/calendar/index.jsx
+++ react-main/src/scenes/calendar/index.jsx
@@ -51,6 +51,40 @@ const MyCalendar = () => {
       <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
       <Box display="flex" justifyContent="space-between">
         {/* CALENDAR SIDEBAR */}
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  margin: "10px 0",
+                  borderRadius: "2px",
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        {/* CALENDAR */}
       </Box>
     </Box>
   );
