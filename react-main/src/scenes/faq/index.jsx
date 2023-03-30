7b66fd26802645d595409067e6fb4a2e977c40ac
alissonkhr <alissonricardo68@gmail.com>
2023-03-29 18:58:36 -0700

    added Header to FAQ

--- react-main/src/scenes/faq/index.jsx
+++ react-main/src/scenes/faq/index.jsx
@@ -7,7 +7,14 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { tokens } from "../../theme";
 
 const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-}
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+    </Box>
+  );
+};
 
 export default FAQ;
