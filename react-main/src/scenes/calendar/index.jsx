4c62c9aec78fbadd460e35837fedd1dd589faa09
alissonkhr <alissonricardo68@gmail.com>
2023-03-29 18:44:46 -0700

    calendar is fully interactive

--- react-main/src/scenes/calendar/index.jsx
+++ react-main/src/scenes/calendar/index.jsx
@@ -89,15 +89,21 @@ const MyCalendar = () => {
         <Box flex="1 1 100%" ml="15px">
           <FullCalendar
             height="75vh"
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
             headerToolbar={{
-              left: "prev, next, today",
+              left: "prev,next today",
               center: "title",
-              right: "dayGridMonth, timeGridWeek, timeGridDay, listMonth",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
             }}
             initialView="dayGridMonth"
             editable={true}
             selectable={true}
+            selectMirror={true}
             dayMaxEvents={true}
             select={handleDateClick}
             eventClick={handleEventClick}
