69856b509437060f6ed4cdebf2daf0e0b0791551
alissonkhr <alissonricardo68@gmail.com>
2023-03-30 23:59:00 -0700

    importing files into barchart

--- react-main/src/components/BarChart.jsx
+++ react-main/src/components/BarChart.jsx
@@ -1,3 +1,8 @@
+import { useTheme } from "@mui/material";
+import { ResponsiveBar } from "@nivo/bar";
+import { tokens } from "../theme";
+import { mockBarData as data } from "../data/mockData";
+
 const BarChart = () => {
   return (
     <ResponsiveBar
