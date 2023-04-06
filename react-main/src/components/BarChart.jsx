b755f7bd832b8e5d288a9463a063a4fab611caf7
alissonkhr <alissonricardo68@gmail.com>
2023-04-05 23:38:07 -0700

    added pie chart file to components with basic set up

--- react-main/src/components/BarChart.jsx
+++ react-main/src/components/BarChart.jsx
@@ -3,7 +3,7 @@ import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
 import { mockBarData as data } from "../data/mockData";
 
-const BarChart = () => {
+const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
@@ -97,7 +97,7 @@ const BarChart = () => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "food",
+        legend: isDashboard ? undefined : "food",
         legendPosition: "middle",
         legendOffset: -40,
       }}
