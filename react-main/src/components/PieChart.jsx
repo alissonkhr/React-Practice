22bed7f3b0f01b6fe19fe51acca6b42052420cfa
alissonkhr <alissonricardo68@gmail.com>
2023-04-10 00:04:46 -0700

    line chart has been populated on page with data

--- react-main/src/components/PieChart.jsx
+++ react-main/src/components/PieChart.jsx
@@ -1,5 +1,5 @@
-import { ResponsivePie } from "@nivo/pie";
 import { useTheme } from "@mui/material";
+import { ResponsivePie } from "@nivo/pie";
 import { tokens } from "../theme";
 import { mockPieData as data } from "../data/mockData";
 
