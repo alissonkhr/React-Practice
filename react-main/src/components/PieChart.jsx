e4cd3432592faaa09e48120c7a4a25570bc7482c
alissonkhr <alissonricardo68@gmail.com>
2023-04-10 00:12:01 -0700

    finally changed the color of the pie chart text

--- react-main/src/components/PieChart.jsx
+++ react-main/src/components/PieChart.jsx
@@ -10,34 +10,34 @@ const PieChart = () => {
   return (
     <ResponsivePie
       data={data}
-      theme={{
-        axis: {
-          domain: {
-            line: {
-              stroke: colors.grey[100],
-            },
-          },
-          legend: {
-            text: {
-              fill: colors.grey[100],
-            },
-          },
-          ticks: {
-            line: {
-              stroke: colors.grey[100],
-              strokeWidth: 1,
-            },
-            text: {
-              fill: colors.grey[100],
-            },
-          },
-        },
-        legends: {
-          text: {
-            fill: colors.grey[100],
-          },
-        },
-      }}
+      // theme={{
+      //   axis: {
+      //     domain: {
+      //       line: {
+      //         stroke: colors.grey[100],
+      //       },
+      //     },
+      //     legend: {
+      //       text: {
+      //         fill: colors.grey[100],
+      //       },
+      //     },
+      //     ticks: {
+      //       line: {
+      //         stroke: colors.grey[100],
+      //         strokeWidth: 1,
+      //       },
+      //       text: {
+      //         fill: colors.grey[100],
+      //       },
+      //     },
+      //   },
+      //   legends: {
+      //     text: {
+      //       fill: colors.grey[100],
+      //     },
+      //   },
+      // }}
       margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
       innerRadius={0.5}
       padAngle={0.7}
@@ -49,12 +49,12 @@ const PieChart = () => {
         modifiers: [["darker", 0.2]],
       }}
       arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextColor="#333333"
+      arcLinkLabelsTextColor= {colors.grey[100]}
       arcLinkLabelsThickness={2}
       arcLinkLabelsColor={{ from: "color" }}
       arcLabelsSkipAngle={10}
       arcLabelsTextColor={{
-        from: "color",
+        from: colors.grey[100],
         modifiers: [["darker", 2]],
       }}
       defs={[
@@ -137,7 +137,7 @@ const PieChart = () => {
           itemsSpacing: 0,
           itemWidth: 100,
           itemHeight: 18,
-          itemTextColor: "#999",
+          itemTextColor: colors.grey[100],
           itemDirection: "left-to-right",
           itemOpacity: 1,
           symbolSize: 18,
@@ -146,7 +146,7 @@ const PieChart = () => {
             {
               on: "hover",
               style: {
-                itemTextColor: "#000",
+                itemTextColor: colors.grey[100],
               },
             },
           ],
