7b0a4e23baa75ab5fb8a284e0a28c41303a60fbf
alissonkhr <alissonricardo68@gmail.com>
2023-04-09 23:17:01 -0700

    added imports for linechart

--- react-main/src/components/LineChart.jsx
+++ react-main/src/components/LineChart.jsx
@@ -1,4 +1,12 @@
+import { useTheme } from "@mui/material";
+import { ResponsiveLine } from "@nivo/line";
+import { tokens } from "../theme";
+import { mockLineData as data } from "../data/mockData";
+
 const LineChart = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <div>
       <div></div>
