84feddc855ed465d7adbd8660f88cda97c5b10fd
alissonkhr <alissonricardo68@gmail.com>
2023-04-16 12:07:00 -0700

    adding basic code to make progress circle component

---
+++ react-main/src/components/ProgressCircle.jsx
@@ -0,0 +1,6 @@
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const ProgressCircle = ({ progress = "0.75", size = "40" }) => {};
+
+export default ProgressCircle;
