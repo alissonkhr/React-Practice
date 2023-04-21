e792f6cbdb6c0ae45fbf3927a4efe179c589e9df
alissonkhr <alissonricardo68@gmail.com>
2023-04-21 12:48:10 -0700

    adding more imports to dashboard that are needed

--- react-main/src/scenes/dashboard/index.jsx
+++ react-main/src/scenes/dashboard/index.jsx
@@ -1,4 +1,17 @@
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import PieChart from "../../components/PieChart";
+import GeographyChart from "../../components/GeographyChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 import Header from "../../components/Header";
 
 const Dashboard = () => {
