27655d6f6979fd3b15a090c4063d661897e0cf71
alissonkhr <alissonricardo68@gmail.com>
2023-04-22 16:15:50 -0700

    added download button to dashboard component

--- react-main/src/scenes/dashboard/index.jsx
+++ react-main/src/scenes/dashboard/index.jsx
@@ -15,10 +15,27 @@ import ProgressCircle from "../../components/ProgressCircle";
 import Header from "../../components/Header";
 
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+      <Box>
+        <Button
+          sx={{
+            backgroundColor: colors.blueAccent[700],
+            color: colors.grey[100],
+            fontSize: "14px",
+            fontWeight: "bold",
+            padding: "10px 20px",
+          }}
+        >
+          <DownloadOutlinedIcon sx={{ mr: "10px/" }} />
+          Download Reports
+        </Button>
       </Box>
     </Box>
   );
