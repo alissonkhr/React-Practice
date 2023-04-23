a509cb7fc3b025ff1fe2d9c85087e6f54ca98e7f
alissonkhr <alissonricardo68@gmail.com>
2023-04-23 12:28:06 -0700

    added mock data statbox on dashboard

--- react-main/src/scenes/dashboard/index.jsx
+++ react-main/src/scenes/dashboard/index.jsx
@@ -37,6 +37,35 @@ const Dashboard = () => {
           Download Reports
         </Button>
       </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
