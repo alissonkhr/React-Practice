17272acb459497f356d35f3cd5ca7e4d28a74708
alissonkhr <alissonricardo68@gmail.com>
2023-04-25 10:13:50 -0700

    moved dashboard button to correct side of page

--- react-main/src/scenes/dashboard/index.jsx
+++ react-main/src/scenes/dashboard/index.jsx
@@ -22,20 +22,21 @@ const Dashboard = () => {
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
-      </Box>
-      <Box>
-        <Button
-          sx={{
-            backgroundColor: colors.blueAccent[700],
-            color: colors.grey[100],
-            fontSize: "14px",
-            fontWeight: "bold",
-            padding: "10px 20px",
-          }}
-        >
-          <DownloadOutlinedIcon sx={{ mr: "10px/" }} />
-          Download Reports
-        </Button>
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px/" }} />
+            Download Reports
+          </Button>
+        </Box>
       </Box>
 
       {/* GRID & CHARTS */}
