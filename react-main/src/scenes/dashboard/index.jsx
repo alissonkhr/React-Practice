e278dbd38e8c3359f37fac1ddf6f1e55b0eab058
alissonkhr <alissonricardo68@gmail.com>
2023-05-01 03:11:26 -0700

    new statbox for other chart added

--- react-main/src/scenes/dashboard/index.jsx
+++ react-main/src/scenes/dashboard/index.jsx
@@ -165,6 +165,31 @@ const Dashboard = () => {
           <Box height="250px" ml="-20px">
             <LineChart isDashboard={true} />
           </Box>
+          {/* TRANSACTIONS */}
+          <Box
+            gridColumn="span 4"
+            gridRow="span 2"
+            backgroundColor={colors.primary[400]}
+            overflow="auto"
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              colors={colors.grey[100]}
+              p="15px"
+            >
+              <Typography
+                color={colors.grey[100]}
+                variant="h5"
+                fontWeight="600"
+              >
+                Recent Transactions
+              </Typography>
+            </Box>
+            
+          </Box>
         </Box>
       </Box>
     </Box>
